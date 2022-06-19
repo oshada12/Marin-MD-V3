3c561bd5cf5f10d8c9ef7e1d26d93ee77052983d
oshada12 <107744144+oshada12@users.noreply.github.com>
2022-06-19 21:47:29 +0100

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ second: 'numeric'
 
 function pickRandom(list) {
   return list[Math.floor(Math.random() * list.length)]
-}global.owner = ['17608914335'] // Put your number here
+}global.owner = ['94775792013'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = [] // Premium user has unlimited limit
 global.APIs = { // API Prefix
@@ -61,19 +61,19 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'Marin-MD'
-global.author = `Ｍａｒｉｎ－ＭＤ
+global.packname = 'Zero-MD'
+global.author = `Z E R O－ＭＤ
 
 
 
 
 
 ig: senpai_chan_gemoy`
-global.wm = 'Ｍａｒｉｎ－ＭＤ'
+global.wm = 'Z E R O－ＭＤ'
 global.watermark = wm
 global.botdate = '╭──────────═┅═─────────\n│◪ ${time}\n┊◪ ${week} ${date}\n╰──────────═┅═─────────'
 global.media = 'https://telegra.ph/file/e001198ced5650ef53d47.jpg'
-global.instagram = 'https://instagram.com/senpai_chan_gemoy'
+global.instagram = 'https://instagram.com/Vimukthi.oshada'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=crafts-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23401620&text='
 global.wait = '_*tunggu sedang di proses...*_'
 global.eror = '_*Server Error*_'
